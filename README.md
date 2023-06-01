9592ddea4960741d175202825b39e1fce420faa8
sunkang <13127955213@qq.com>
2023-06-01 14:01:07 +0800

    提交第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# rpc_php
+# 安装
+
+在composer.json中添加
+~~~
+"thefunpower/rpc_php": "dev-main" 
+~~~
+ 
+## RPC
+
+服务端
+~~~
+class ServerGetUser{
+    public function getInfo($name = 'abc'){
+        return ['welcome'=>$name,'token'=>rpc_token()];
+    }
+}
+rpc_server("ServerGetUser");
+~~~
+
+客户端
+~~~
+$client = rpc_client("http://127.0.0.1:5000/rpc.php");
+$info = $client->getInfo("test");
+print_r($info);
+~~~ 
+ 
+
+
+
+### 开源协议 
+
+The [MIT](LICENSE) License (MIT)
---
+++ helper.php
@@ -0,0 +1,67 @@
+<?php   
+/*
+* Copyright (c) 2021-2031, All rights reserved.
+* MIT LICENSE
+*/ 
+if(!function_exists('rpc_client')){
+    function rpc_client($url,$is_remote = false){
+        return rpc_php::client($url,$is_remote);
+    }
+}
+
+if(!function_exists('rpc_server')){
+    function rpc_server($class){
+        rpc_php::server($class);
+    }
+}
+
+if(!function_exists('rpc_token')){
+    function rpc_token(){
+        return rpc_php::get_http_author();
+    }
+}
+   
+class rpc_php{  
+    
+    public static $token;
+    /**
+    * 取TOKEN
+    */
+    public static function get_http_author(){
+        return trim(substr($_SERVER['HTTP_AUTHORIZATION'],strlen('Bearer')));
+    } 
+    /**
+    * 客户端请求
+    */
+    public static function client($remote_url,$is_remote = false){
+        $client = new \Yar_Client($remote_url);   
+        $opt = [
+            "Authorization: Bearer ".self::$token,  
+        ]; 
+        $client->SetOpt(YAR_OPT_HEADER, $opt); 
+        if(!$is_remote){
+            $host = $remote_url;
+            $host = str_replace("https://","",$host);
+            $host = str_replace("http://","",$host);
+            $host = substr($host,0,strpos($host,'/'));  
+            if(strpos($host,'127.0.0.1') === false){
+                $port = '80';
+                $client->SetOpt(YAR_OPT_RESOLVE, array("$host:$port:127.0.0.1"));           
+            }           
+        }       
+        if($token && YAR_VERSION >= '2.3.0'){
+            $client->setOpt(YAR_OPT_PROVIDER, "provider");
+            $client->setOpt(YAR_OPT_TOKEN, $token);   
+        } 
+        $client->SetOpt(YAR_OPT_PERSISTENT, 1);
+        $client->SetOpt(YAR_OPT_CONNECT_TIMEOUT, 6000);  
+        return $client;
+    }
+    /**
+    * 服务端
+    */
+    public static function server($class){ 
+        $service = new \Yar_Server(new $class());
+        $service->handle();
+    }
+}
